61e78b89095303bf1d111a0286333406c9b21ebf
Maryan-Gazduk <90209398+Maryan-Gazduk@users.noreply.github.com>
2022-04-22 20:58:46 +0300

    test_commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const password = document.getElementById('password')
 const toggle = document.getElementById('toggle')
+
 function showHide() {
     if(password.type === 'password'){
         password.setAttribute('type', 'text')
